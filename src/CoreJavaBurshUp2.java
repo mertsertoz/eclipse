9c57f86564bb5201a7d5f10fdd8db806e7372d90
Mert Sertöz <mert.sertoz@live.com>
2022-12-02 15:43:37 +0300

    if,else,break

---
+++ src/CoreJavaBurshUp2.java
@@ -0,0 +1,31 @@
+
+public class CoreJavaBurshUp2 {
+
+	public static void main(String[] args) {
+		
+		int[] ary2 = {4,5,6,7,8,52,48,66,31};
+		
+		for(int i=0; i<ary2.length; i++)
+			
+		{
+			if(ary2[i] % 2 == 0)
+			{	
+				System.out.println(ary2[i]);
+			}
+			else 
+			{
+				System.out.println(ary2[i] + "its not multiple of 2");
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
