5d98f9919a3daa977a5a98283df3ed63c13cbf24
Mert Sertöz <mert.sertoz@live.com>
2022-12-02 12:00:40 +0300

    second commit

---
+++ src/CoreJavaBrushUp1.java
@@ -0,0 +1,34 @@
+
+public class CoreJavaBrushUp1 {
+
+	public static void main(String[] args) {
+
+         int myNum = 5;
+         String website = "Rahul Shetty Academy";
+         char letter = 'm';
+         double decimal2 = 2.3233;
+         boolean myCard = true;
+         
+         System.out.println(myNum);
+        //Arrays     
+         
+	    int[] ary = new int[5];
+	    ary[0] = 1;
+	    ary[1] = 2;
+	    ary[2] = 4;
+	    ary[3] = 9;
+	    ary[4] = 7;
+	    
+	    int[] ary2 = {4,5,6,7,8};
+	    
+	    System.out.println(ary2[2]);
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+
+}
