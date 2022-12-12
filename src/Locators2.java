bf195c0513e30c88a23f2c2e4910b34775781322
Mert Sertöz <mert.sertoz@live.com>
2022-12-12 11:40:55 +0300

    locators2

--- src/Locators2.java
+++ src/Locators2.java
@@ -14,9 +14,10 @@ public class Locators2 {
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		Thread.sleep(2000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
@@ -26,7 +27,28 @@ public class Locators2 {
 		driver.close();
 		
 		
-		
 	}
 
+    public static String getPassword(WebDriver driver) throws InterruptedException	
+	
+    {
+    	driver.get("https://rahulshettyacademy.com/locatorspractice/");
+    	driver.findElement(By.linkText("Forgot your password?")).click();
+    	Thread.sleep(1000);
+    	driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText =driver.findElement(By.cssSelector("form p")).getText();
+		//Please use temporary password 'rahulshettyacademy' to Login.
+		String[] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'") [0];
+		return password;
+    	//0th index - Please use temporary password
+		//1st index - rahulshettyacademy' to Login.
+    	
+		//0th index - rahulshettyacademy
+		//1st index - to Login.
+		
+		
+    }
+	
+	
 }
