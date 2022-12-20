5117eb6349ed7a2618fdc783262f23f5875befbb
Mert Sertöz <mert.sertoz@live.com>
2022-12-20 16:13:43 +0300

    alerts

---
+++ src/Assignment.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		
+		
+		String text = "Mert";
+		System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys(text);
+		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		driver.findElement(By.id("confirmbtn")).click();
+		driver.switchTo().alert().dismiss();
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
