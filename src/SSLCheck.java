9c6f1c5d7a4bf6b47555855f9ff79c82a9c87acc
Mert Sertöz <mert.sertoz@live.com>
2023-01-17 10:03:38 +0300

    proxy, download folder choosing, ssl

--- src/SSLCheck.java
+++ src/SSLCheck.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -5,11 +8,21 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class SSLCheck {
 
 	public static void main(String[] args) {
-		
-		ChromeOptions options = new  ChromeOptions();
+
+		ChromeOptions options = new ChromeOptions();
+		// options.addExtensions("path"); for opening browser with extensions
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("proxy adress");
+		options.setCapability("proxy", proxy);
+		Map<String, Object> prefs = new HashMap<String, Object>();
+
+		prefs.put("download.default_directory", "/directory/path");
+
+		options.setExperimentalOption("prefs", prefs);  //download to spesific folder
+
 		options.setAcceptInsecureCerts(true);
 		System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
-		WebDriver driver= new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://expired.badssl.com/");
 		System.out.println(driver.getTitle());
 	}
