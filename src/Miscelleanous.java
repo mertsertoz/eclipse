9d5e4ec22b7c36b2a46bd1ce98501bab1ed88e69
Mert Sertöz <mert.sertoz@live.com>
2023-01-17 10:55:35 +0300

    taking screenshots with selenium

---
+++ src/Miscelleanous.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Miscelleanous {
+
+	public static void main(String[] args) throws IOException  {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		//driver.manage().deleteCookieNamed("sessionKey");
+		
+		
+		
+		driver.get("https://www.google.com.tr");
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src,new File("D:\\Step1.png"));
+		
+		
+	}
+
+}
