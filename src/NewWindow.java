14c83a5e5171071fda214b3c8b65db22411ebbdd
Mert Sertöz <mert.sertoz@live.com>
2023-01-31 17:34:29 +0300

    inkoving new windows

---
+++ src/NewWindow.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewWindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.rahulshettyacademy.com/angularpractice/");
+		driver.switchTo().newWindow(WindowType.TAB);
+		Set<String> handles = driver.getWindowHandles();
+		Iterator<String> it = handles.iterator();
+		String parentWindowId = it.next();
+		String childWindowId = it.next();
+		driver.switchTo().window(childWindowId);
+		driver.get("https://www.rahulshettyacademy.com/");
+		
+	}
+
+}
