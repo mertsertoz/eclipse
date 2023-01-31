ad73ce32dcf1d292d1ec19cf87c3c276899dce9a
Mert Sertöz <mert.sertoz@live.com>
2023-01-31 17:45:32 +0300

    webelement screenshot

--- src/NewWindow.java
+++ src/NewWindow.java
@@ -1,13 +1,19 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -20,6 +26,21 @@ public class NewWindow {
 		String childWindowId = it.next();
 		driver.switchTo().window(childWindowId);
 		driver.get("https://www.rahulshettyacademy.com/");
+		Thread.sleep(2000);
+		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
+		driver.switchTo().window(parentWindowId);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		File file= name.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file,new File("logo.png"));
+		
+		
+		
+		
+		
+		//driver.quit();
+		
+		
 		
 	}
 
