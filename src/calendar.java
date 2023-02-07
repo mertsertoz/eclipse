114dee722188946dd5e96d8f8587ddbe88238ff3
Mert Sertöz <mert.sertoz@live.com>
2023-02-07 23:02:24 +0300

    updated

--- src/calendar.java
+++ src/calendar.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,29 +16,44 @@ public class calendar {
 
 	{
 
-		String url ="https://www.path2usa.com/travel-companion";
+		String url = "https://www.path2usa.com/travel-companion";
 
 		WebDriver driver;
 
-		
-
 		System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
-
+		
 		driver = new ChromeDriver();
-
+		
+		
 		driver.get(url);
-
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 
 		// Scroll the Page
 
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 
 		js.executeScript("window.scrollBy(0,1100)");
 
-		// Click on the Date field
+		Thread.sleep(2000);
 
-		Thread.sleep(3000);
+		WebElement name = driver.findElement(By.id("form-field-travel_from"));
+		
+		name.sendKeys("Las");
+		
+		Thread.sleep(2000);
+		WebElement nameDrop = driver.findElement(By.cssSelector("li[class='ui-menu-item']"));
+		nameDrop.click();
+		/*List<WebElement> dropLists = driver.findElements(By.cssSelector("li[class='ui-menu-item']"));
+
+		for (WebElement dropList : dropLists) {
+			if (dropList.getText().contains("Las Vegas")) {
+				dropList.click();
+				break;
+			}
+		}*/
+
+		
 
 		WebElement Date = driver.findElement(By.xpath("//*[@id='form-field-travel_comp_date']"));
 
@@ -45,48 +61,47 @@ public class calendar {
 
 		Date.click();
 
-		//WebElement month = driver.findElement(By.xpath("//div[@class='flatpickr-month']"));
+		// WebElement month =
+		// driver.findElement(By.xpath("//div[@class='flatpickr-month']"));
 
-		while(!driver.findElement(By.xpath("//div[@class='flatpickr-month']")).getText().contains("April"))
+		while (!driver.findElement(By.xpath("//div[@class='flatpickr-month']")).getText().contains("April"))
 
 		{
 
-		Thread.sleep(2000);
+			Thread.sleep(2000);
 
-		     driver.findElement(By.xpath("//span[@class='flatpickr-next-month']")).click();
+			driver.findElement(By.xpath("//span[@class='flatpickr-next-month']")).click();
 
 		}
 
-		int count= driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).size();
+		int count = driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).size();
 
-		for(int i=0;i<count;i++)
+		for (int i = 0; i < count; i++)
 
 		{
 
-		//Thread.sleep(3000);
+			 
 
-		     String text=driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).getText();
+			String text = driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).getText();
 
-		     if(text.equalsIgnoreCase("26"))
+			if (text.equalsIgnoreCase("26"))
 
-		     {
+			{
 
-		   
+				driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).click();
 
-		driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).click();
+				System.out.println("The select day is:" + text);
 
-		    System.out.println("The select day is:"+text);
+				break;
 
-		    break;
-
-		     }
+			}
 
 		}
 
 		String date = driver.findElement(By.xpath("//*[@id='form-field-travel_comp_date']")).getAttribute("value");
 
-		System.out.println("The select date is:"+date);
+		System.out.println("The select date is:" + date);
 
-		}
+	}
 
-		}
+}
